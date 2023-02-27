c5d3b0f39b34eb9945e4247ac2df515954b7d520
Diego Maldonado <108159490+diegomaldonadofrontdev@users.noreply.github.com>
2023-02-27 12:06:48 -0300

    commit 1

--- home/Homework.js
+++ home/Homework.js
@@ -1,15 +1,19 @@
-function laCajaDePandora(numero){
-// proximamente escribiremos codigo aqui
-if (numero % 2 === 0) 
-{return ( numero.toString(2))} 
-else {return (numero.toString(16))}
+function laCajaDePandora(numero) {
+	// proximamente escribiremos codigo aqui
+	if (numero % 2 === 0) {
+		let num = numero;
+		let binary = (num % 2).toString();
+		for (; num > 1; ) {
+			num = parseInt(num / 2);
+			binary = (num % 2) + binary;
+		}
+		return binary;
+	}
+
+	return numero.toString(16);
 }
 
-function Nito(){
-    return 
-    {Name: Nito;
-     Age: 18;
-     Nacionality: Argentinian;
-    }
+function diegoMaldonado() {
+	return { nombre: "Diego", apellido: "Maldonado", nacionalidad: "Argentino" };
 }
 
