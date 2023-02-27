0882807598a71dd2d6c65b20058eef6e289d3adb
ElQuebeck <115952174+ElQuebeck@users.noreply.github.com>
2023-02-27 12:16:08 -0300

    Agregue function laCajadePandora

--- home/Homework.js
+++ home/Homework.js
@@ -1,3 +1,14 @@
-function laCajaDePandora(numero){
-// proximamente escribiremos codigo aqui
+function laCajaDePandora(numero) {
+	// proximamente escribiremos codigo aqui
+	if (numero % 2 === 0) {
+		let num = numero;
+		let binary = (num % 2).toString();
+		for (; num > 1; ) {
+			num = parseInt(num / 2);
+			binary = (num % 2) + binary;
+		}
+		return binary;
+	}
+
+	return numero.toString(16);
 }
