1b2424f19bf26fb6f0b9b3e72f94194ca97ff58a
N1t4z0 <agusdelpercio@gmail.com>
2023-02-27 11:23:59 -0300

    agrege la funcion y hice funcionar la otra

--- home/Homework.js
+++ home/Homework.js
@@ -1,3 +1,14 @@
 function laCajaDePandora(numero){
 // proximamente escribiremos codigo aqui
+if (numero % 2 === 0) 
+{return ( numero.toString(2))} 
+else {return (numero.toString(16))}
+}
+
+function Nito(){
+    return 
+    {Name: Nito;
+     Age: 18;
+     Nacionality: Argentinian;
+    }
 }
