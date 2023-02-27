41be5e0e530b1850b7df8af0f7ef15463a5e1197
Diego Maldonado <108159490+diegomaldonadofrontdev@users.noreply.github.com>
2023-02-26 21:29:02 -0300

    funcion con mi nombre

--- home/Homework.js
+++ home/Homework.js
@@ -1,15 +1,18 @@
 function laCajaDePandora(numero) {
-    // proximamente escribiremos codigo aqui
-    if (numero % 2 === 0) {
-      let num = numero;
-      let binary = (num % 2).toString();
-      for (; num > 1; ) {
-        num = parseInt(num / 2);
-        binary = (num % 2) + binary;
-      }
-      return binary;
-    }
-  
-    return numero.toString(16);
-  }
-  
+	// proximamente escribiremos codigo aqui
+	if (numero % 2 === 0) {
+		let num = numero;
+		let binary = (num % 2).toString();
+		for (; num > 1; ) {
+			num = parseInt(num / 2);
+			binary = (num % 2) + binary;
+		}
+		return binary;
+	}
+
+	return numero.toString(16);
+}
+
+function diegoMaldonado() {
+	return { nombre: "Diego", apellido: "Maldonado", nacionalidad: "Argentino" };
+}
